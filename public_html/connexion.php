8925e6da573d5f591a4c615eb5ed2629eaaf8d74
Duthis47 <ducrot.mathis47@gmail.com>
2025-10-30 23:46:21 +0100

    Refonte architecture pour prévoir sécurité

---
+++ public_html/connexion.php
@@ -0,0 +1,12 @@
+<?php
+
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
+ */
+
+$dsn = "";
+$username="";
+$passwd = "";
+
+$laConnexion = new PDO($dsn, $username, $passwd);
